c9d680f5862d19512209381d2d679a7afd9d1c17
Tauan Mayk <tauanmaykreal@gmail.com>
2025-05-06 19:19:04 -0300

    ajustando o css e o tsx de varios arquivos

--- src/components/PageStyle/PageStyle.tsx
+++ src/components/PageStyle/PageStyle.tsx
@@ -1,53 +1,60 @@
 import ZeldaGames from "../ZeldaGames/ZeldaGames";
-import "./pagestyle.css"
+import "./pagestyle.css";
 
 const PageStyle = () => {
-    return (
-        <>
-            <header className="page-header">
-                <div>
-                    <img srcSet="../../src/images/pngegg.png" alt="logo" className="logo"/>
-                </div>
-                <ul className="page-header-menu">
-                    <li>
-                        <a href="#">Home</a>
-                    </li>
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#">Games</a>
-                    </li>
-                    <li>
-                        <a href="#">Publishers</a>
-                    </li>
-                </ul>
-            </header>
-            <main className="page-main " id="page-main">
-                <div className="sombra-interna">
-                <img srcSet="../../src/images/the-legend-of-zelda-landscape-sword-5qvpc6rnsmwsdqbd.jpg" alt="background" className="page-main-background"/>
-                </div>
-                <div className="page-main-content">
-                    <h3>The legend of Zelda</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
-                    <button className="page-main-button">
-                        <a href="#games">See more</a>
-                    </button>
-                </div>
-            </main>
-            <section className="page-games-section" id="games">
-                <h2>Games</h2>
-                <div className="page-games-section-games">
-                    <ZeldaGames />
-                </div>
-            </section>
-            <footer className="page-footer">
-                <div className="page-footer-content">
-                    <p>Copyright © 2025 Zelda Games</p>
-                </div>
-            </footer>
-        </>
-    )
-}
+  return (
+    <>
+      <header className="page-header">
+        <div>
+          <img srcSet="./src/images/pngegg.png" alt="logo" className="logo" />
+        </div>
+        <ul className="page-header-menu">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Games</a>
+          </li>
+          <li>
+            <a href="#">Publishers</a>
+          </li>
+        </ul>
+      </header>
+      <main className="page-main " id="page-main">
+        <div className="sombra-interna">
+          <img
+            srcSet="./src/images/the-legend-of-zelda-landscape-sword-5qvpc6rnsmwsdqbd.jpg"
+            alt="background"
+            className="page-main-background"
+          />
+        </div>
+        <div className="page-main-content">
+          <h3>The legend of Zelda</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            quos.
+          </p>
+          <button className="page-main-button">
+            <a href="#games">See more</a>
+          </button>
+        </div>
+      </main>
+      <section className="page-games-section" id="games">
+        <h2>Games</h2>
+        <div className="page-games-section-games">
+          <ZeldaGames />
+        </div>
+      </section>
+      <footer className="page-footer">
+        <div className="page-footer-content">
+          <p>Copyright © 2025 Zelda Games</p>
+        </div>
+      </footer>
+    </>
+  );
+};
 
 export default PageStyle;
--- src/components/ZeldaGames/ZeldaGames.tsx
+++ src/components/ZeldaGames/ZeldaGames.tsx
@@ -1,26 +1,33 @@
 import dados from "../../../games.json";
-import "./zeldagames.css"; 
+import "./zeldagames.css";
 
 const ZeldaGames = () => {
-    return (
+  return (
     <>
-        <div className="zelda-games-section">
-            <ul className="zelda-games-list">
-                {dados.jogos.map((game: {nome: string, imagem: string, descricao: string, desenvolvedor: string, publicador: string}) => (
-                    <li key={game.nome} className="zelda-games-list-item">
-                        <img src={game.imagem} alt={game.nome} />
-                        <p className="nome-game">{game.nome}</p>
-                        <div className="informacoes-games">
-                            {/* <p className="descricao-games">{game.descricao}</p> */}
-                            <p className="developers">{game.desenvolvedor}</p>
-                            <p className="publishers">{game.publicador}</p>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </div>
+      <div className="zelda-games-section">
+        <ul className="zelda-games-list">
+          {dados.jogos.map(
+            (game: {
+              nome: string;
+              imagem: string;
+              descricao: string;
+              desenvolvedor: string;
+              publicador: string;
+            }) => (
+              <li key={game.nome} className="zelda-games-list-item">
+                <img src={game.imagem} alt={game.nome} />
+                <p className="nome-game">{game.nome}</p>
+                <div className="informacoes-games">
+                  <p className="developers">{game.desenvolvedor}</p>
+                  <p className="publishers">{game.publicador}</p>
+                </div>
+              </li>
+            )
+          )}
+        </ul>
+      </div>
     </>
-    )
-}
+  );
+};
 
 export default ZeldaGames;
