861220197d4787e33f64daefbc427a94217ffeba
Tauan Mayk <tauanmaykreal@gmail.com>
2025-05-09 16:04:58 -0300

    Iniciando o proximo json com monstros com a api de zelda

--- src/API/zelda-api.py
+++ src/API/zelda-api.py
@@ -1,35 +1,64 @@
 import requests
 import json
 
-def fetch_data(endpoint, filters={}):
+# def fetch_data(endpoint, filters={}):
+#     url = f"https://zelda.fanapis.com/api/{endpoint}"
+#     response = requests.get(url, params=filters)
+    
+#     return response.json() if response.status_code == 200 else None
+
+# characters = fetch_data("games", {"limit": 10})
+
+# jogos = [
+#         {"nome": game["name"],
+#         "imagem": f"./src/images/gamesimages/gamesimages{i+1}.avif",
+#         "descricao": game["description"],
+#         "desenvolvedor": game["developer"],
+#         "publicador": game["publisher"],
+#          } for i, game in enumerate(characters["data"] if characters else [])
+#         ]
+
+# # Montar o dicionário final
+# dados = {
+#     "jogos": jogos
+# }
+
+# # Salvar em JSON
+# with open("games.json", "w", encoding="utf-8") as file:
+#     json.dump(dados, file, indent=4, ensure_ascii=False)
+
+
+def fetch_data2(endpoint, filters={}):
     url = f"https://zelda.fanapis.com/api/{endpoint}"
     response = requests.get(url, params=filters)
     
     return response.json() if response.status_code == 200 else None
 
-characters = fetch_data("games", {"limit": 10})
+Monters = fetch_data2("monster", {"limit": 10})
 
-jogos = [
-        {"nome": game["name"],
-        "imagem": f"./src/images/gamesimages/gamesimages{i+1}.avif",
-        "descricao": game["description"],
-        "desenvolvedor": game["developer"],
-        "publicador": game["publisher"],
-         } for i, game in enumerate(characters["data"] if characters else [])
+monstros = []
+
+if Monters and "data" in Monters:
+    monstros = [
+    {
+        "nome": monster["name"],
+        "descricao": monster["description"],
+        "aparicoes": [
+            {
+                "nome": appearance["name"],
+                "data-de-lancamento": appearance["release_date"]
+            }
+            for appearance in monster.get("appearances", [])
         ]
+    } for monster in Monters["data"]
+]
 
-# Montar o dicionário final
-dados = {
-    "jogos": jogos
+
+dados2 = {
+    "monstros": monstros
 }
 
-# Salvar em JSON
-with open("games.json", "w", encoding="utf-8") as file:
-    json.dump(dados, file, indent=4, ensure_ascii=False)
+print(Monters)
 
-
-# "descricao": game["description"],
-# "data_lancamento": game["release_date"],
-# "plataformas": game["platforms"],
-# "desenvolvedor": game["developer"],
-# "publicador": game["publisher"],
+with open("monsters.json", "w", encoding="utf-8") as file:
+    json.dump(dados2, file, indent=4, ensure_ascii=False)
