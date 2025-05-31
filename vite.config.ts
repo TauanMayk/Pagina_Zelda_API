9ad4c9cb928a3bdb05837d34feba70fae81704cf
Tauan Mayk <tauanmaykreal@gmail.com>
2025-05-31 15:22:14 -0300

    vite de novo

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Pagina_Zelda_API/', 
   plugins: [
     tailwindcss(),
     react()
@@ -13,4 +12,5 @@ export default defineConfig({
   build: {
     outDir: 'dist'
   },
+  base: './', 
 })
